b8e7829e8029ef0904dd0a8869bc1fceeecc757b
PavanKarthikGaraga <pavankarthik107@gmail.com>
2025-05-09 10:35:38 +0530

    feat: removed dup next file

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['socialinternship.kluniversity.in'],
+    },
+    allowedDevOrigins: [
+        'socialinternship.kluniversity.in',
+        'https://socialinternship.kluniversity.in'
+    ],
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: 'https://socialinternship.kluniversity.in',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                    },
+                ],
+            },
+        ]
+    },
+};
 
 export default nextConfig;
