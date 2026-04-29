902d1e8b760c9007d49c7d833bf81a2abe9cf128
Nischal Singana <singananischal@gmail.com>
2026-04-29 21:18:03 +0530

    chore: update footer year and credits

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -3,8 +3,8 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="dashboard-footer">
-      <p>© 2024 Smart Village Revolution. All Rights Reserved.</p>
-      <p>Designed and Developed by Pavan Karthik Garaga | ZeroOne CodeClub</p>
+      <p>© 2026 Smart Village Revolution. All Rights Reserved.</p>
+      <p>Designed and Developed by ZeroOne CodeClub</p>
     </footer>
   );
 } 
