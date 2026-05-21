e3e24eabd7461e16a0a24e2c9bf801aee9c2a9b4
Nischal Singana <singananischal@gmail.com>
2026-05-21 16:16:40 +0530

    chore: remove unnecessary test and scratch files

--- my-app/check_student.js
+++
@@ -1,7 +0,0 @@
-import db from './src/lib/db.js';
-async function test() {
-  const [rows] = await db.query('SELECT * FROM registrations WHERE username = "2500030057"');
-  console.log('2500030057:', rows[0]);
-  process.exit(0);
-}
-test();
